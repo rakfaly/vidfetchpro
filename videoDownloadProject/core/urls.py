464013688277eb21191af2adb78460e981bf21b4
rakfaly <rakfaly@gmail.com>
2026-02-07 16:37:37 +0100

    remove outside .gitignore

--- videoDownloadProject/core/urls.py
+++ videoDownloadProject/core/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
